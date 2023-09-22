fd769d536803d3af30899976090f4ed0c7ef7af2
Senomaris <109620160+Senomaris@users.noreply.github.com>
2023-09-22 02:04:22 -0300

    Update videoMerger.py

--- videoMerger.py
+++ videoMerger.py
@@ -12,14 +12,14 @@ def combineVideos (video_files, title):
     final_clip.write_videofile(title)
 
 
-
-#
+#Changes the working directory to where the script is.
 script_directory = os.path.dirname(__file__)
 os.chdir(script_directory)
-files = os.listdir()
+
+files = os.listdir() #lists all files in the directory.
 videos = []
 for file in files:
-    if file.endswith(".mp4"):
+    if file.endswith(".mp4"): #add all videos with a mp4 extension to the video list.
         videos.append(file)
 
 combineVideos(videos,"Final.mp4")
